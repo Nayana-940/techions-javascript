22dad0f48ea60ecbc4b7fdeacf3c69efed5529ae
Nayana_940 <nayananarayan940@gmail.com>
2025-01-07 11:40:12 +0530

    naming conversion

---
+++ learning/file 1-example.js
@@ -0,0 +1,10 @@
+// Variable in camelCase
+let userName = "Alice";
+let userAge = 25;
+
+// Function in camelCase
+function greetUser() {
+    console.log("Hello, " + userName + "!");
+}
+
+greetUser();  // Output: Hello, Alice!
